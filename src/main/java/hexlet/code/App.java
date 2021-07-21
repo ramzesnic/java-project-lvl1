89fad5426b29dbd8fcd8273d325e04d3c50250d0
Roman <ramzesnic@gmail.com>
2021-07-21 15:11:07 +0300

    step 3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,16 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Welcome to the Brain Games!");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("May I have your name? ");
+
+    String name = sc.nextLine();
+    System.out.println("Hello, " + name + "!");
+    sc.close();
   }
 }
